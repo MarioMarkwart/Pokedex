7eab1aa347b1618006daeeb7a45f4c0af3ec4b7a
Mario Markwart <mario.markwart@gmail.com>
2024-01-19 07:51:57 +0100

    Name firstletter to Caps

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let currentPokemon;
+let currentPokemonName;
 
 // GIF: document.getElementById('pokemonImage').src = currentPokemon['sprites']['other']['showdown']['front_shiny']
 
@@ -6,6 +7,7 @@ async function loadPokemon(){
     let url = 'https://pokeapi.co/api/v2/pokemon/pikachu'
     let response = await fetch(url);
     currentPokemon = await response.json();
+    currentPokemonName = currentPokemon['name'].charAt(0).toUpperCase() + currentPokemon['name'].slice(1);
 
     console.log(currentPokemon);
     renderPokemonInfo(currentPokemon);
@@ -15,15 +17,19 @@ async function loadPokemon(){
 }
 
 function renderPokemonInfo(){
-    document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
+    document.getElementById('pokemonName').innerHTML = currentPokemonName;
     // document.getElementById('pokemonImage').src = currentPokemon['sprites']['front_shiny'];
     document.getElementById('pokemonImage').src = currentPokemon['sprites']['other']['official-artwork']['front_shiny'];
-    
 }
+
 function setFavIcon(){
     document.getElementById('favicon').href = currentPokemon['sprites']['other']['official-artwork']['front_shiny'];
 }
 
 function setTitle(){
-    document.getElementById('title').innerHTML = `Pokédex - ${currentPokemon['name']}`
+    let title = document.getElementById('title');
+
+    if (currentPokemon == "") title.innerHTML = "Pokédex"
+    else title.innerHTML = `Pokédex - ${currentPokemonName}`;
+
 }
