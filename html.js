ab8dfbd1d19fd66c64d20443e76062018d9b3062
Mario Markwart <mario.markwart@gmail.com>
2024-02-01 15:34:06 +0100

    renderAboutStatsHTML

--- html.js
+++ html.js
@@ -28,7 +28,7 @@ function renderAboutStatHTML(statsTable) {
         <div class="statBody" id="abilities"></div>`;
 
 	toTempArray(pokemonInformations["abilities"][pokemonIndex], "ability", "name").forEach((e) => {
-		document.getElementById('abilities').innerHTML += `<div>${e}</div>`;
+		document.getElementById('abilities').innerHTML += `<div class="statValue">${e}</div>`;
 	});
 
 	statsTable.innerHTML += `
@@ -36,21 +36,21 @@ function renderAboutStatHTML(statsTable) {
         <div class="statBody" id="height"></div>`;
 
     document.getElementById('height').innerHTML = 
-        `<div>${(pokemonInformations["height"][pokemonIndex] / 10).toFixed(2)} cm</div>`
+        `<div class="statValue">${(pokemonInformations["height"][pokemonIndex] / 10).toFixed(2)} cm</div>`
         
         statsTable.innerHTML += ` 
         <div class="statTitle">Weight</div>
         <div class="statBody" id="weight"></div>`;
 
     document.getElementById('weight').innerHTML += 
-        `<div>${pokemonInformations["weight"][pokemonIndex] / 10} kg</div>`
+        `<div class="statValue">${pokemonInformations["weight"][pokemonIndex] / 10} kg</div>`
 
     statsTable.innerHTML +=`
         <div class="statTitle">Type</div>
         <div class="statBody" id="type"></div>`;
 
 	toTempArray(pokemonInformations["types"][pokemonIndex],"type","name").forEach((e) => {
-        statsTable.innerHTML += `<div>${e}</div>`;
+        document.getElementById('type').innerHTML += `<div class="statValue">${e}</div>`;
 	});
 }
 
