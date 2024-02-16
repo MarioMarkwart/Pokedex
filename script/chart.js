b2ca80ce1b2ba25fd9a4e3fa4d46ffd5c086993f
Mario Markwart <mario.markwart@gmail.com>
2024-02-16 07:14:41 +0100

    chartJs

--- script/chart.js
+++ script/chart.js
@@ -2,46 +2,71 @@ let chartData = [];
 let chartLabel = [];
 
 const BG_COLORS = [
-	"rgba(255,99,132,0.2)",
-	"rgba(54,162,235,0.2)",
-	"rgba(255,206,86,0.2)",
-	"rgba(75,192,192,0.2)",
-	"rgba(153,102,255,0.2)",
-	"rgba(255,159,64,0.2)",
+	"rgba(255,99,132,0.8)",
+	"rgba(54,162,235,0.8)",
+	"rgba(255,206,86,0.8)",
+	"rgba(75,192,192,0.8)",
+	"rgba(153,102,255,0.8)",
+	"rgba(255,159,64,0.8)",
 ];
 
 function drawChart() {
-
 	const ctx = document.getElementById("myChart");
 	new Chart(ctx, {
-		type: 'bar',
+		type: "bar",
 		data: {
-		//   labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-		labels: chartLabel,
-		  datasets: [{
-			backgroundColor: BG_COLORS,
-			//   label: '# of Votes',
-			  data: chartData,
-			  borderWidth: 1,
-			  categoryPercentage: 1,
-			}]
+			//   labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+			// labels: chartLabel,
+			labels: ["HP", "Attack", "Defense", "Sp. Atk", "Sp. Def", "Speed"],
+			datasets: [
+				{
+					backgroundColor: BG_COLORS,
+					//   label: '# of Votes',
+					data: chartData,
+					borderWidth: 1,
+					categoryPercentage: 0.8,
+				},
+			],
 		},
 		options: {
 			plugins: {
-				legend:{
-					display: false,
+				legend: {
+				  display: false,
+				},
+				tooltip: {
+				  position: "average",
+				  callbacks: {
+					label: function (tooltipItem) {
+					  return tooltipItem.formattedValue;
+					},
+					title: function (tooltipItems, data) {
+					  return null;
+					},
+				  },
+				  displayColors: false,
+				  labels: {
+					color: "white",
+				  },
+				  position: "average",
+				  caretPadding: 10,
+				},
+			  },
+			responsive: true,
+			indexAxis: "y",
+			animation: true,
+			scales: {
+				y: {
+					beginAtZero: true,
+					grid: {
+						display: false,
+					  },
+				},
+				x:{
+					max: 120,
 				}
 			},
-		responsive: true,
-		indexAxis: 'y',
-		animation: true,
-		  scales: {
-			y: {
-			  beginAtZero: true
-			}
-		  }
-		}
-	  });
+		},
+	});
 }
 
 
